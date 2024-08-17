2b37bf53ff75980d0a19674c39562ab7bf1b0103
GenAnderson <genchung1@outlook.com>
2024-08-16 15:55:55 -1000

    completed search function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Product from "./components/product";
 import banner from "./Banner.png";
 import productsList from "./products.json";
@@ -9,12 +9,30 @@ function App() {
   const [searchedWord, setSearchedWord] = useState("");
   const [allProducts, setAllProducts] = useState(productsList);
 
+  const filteredProducts = (wordsSearched) => {
+    const filtered = productsList.filter((product) =>
+      product.name.toLowerCase().includes(wordsSearched.toLowerCase())
+    );
+    setAllProducts(filtered);
+  };
+
+  useEffect(() => {
+    filteredProducts(searchedWord);
+  }, [searchedWord]);
+
   return (
-    <div className="App" style={{ backgroundColor: "#92b4a7" }}>
-      <Container style={{ backgroundColor: "white" }} className="text-center">
+    <div
+      className="App"
+      style={{ backgroundColor: "#92b4a7", minHeight: "100vh" }}
+    >
+      <Container
+        style={{ backgroundColor: "white" }}
+        className="text-center p-4"
+      >
         <img
           src={banner}
           alt="I need this now banner"
+          style={{ height: "10rem" }}
           className={"mx-auto"}
         ></img>
         <form className={"d-flex justify-content-center mb-4"}>
@@ -27,7 +45,6 @@ function App() {
           ></input>
         </form>
         <Row className="d-flex justify-content-center">
-          {searchedWord}
           {allProducts.map((product) => (
             <Product product={product} />
           ))}
