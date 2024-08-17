67f44eeb965b22129220c0c29acc186448ca40ce
GenAnderson <genchung1@outlook.com>
2024-08-16 15:43:13 -1000

    finished layou

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,36 @@ import { useState } from "react";
 import Product from "./components/product";
 import banner from "./Banner.png";
 import productsList from "./products.json";
-import "./App.css";
 
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 
 function App() {
   const [searchedWord, setSearchedWord] = useState("");
-  const [allProducts, setAllProducts] = useState([productsList]);
+  const [allProducts, setAllProducts] = useState(productsList);
 
   return (
-    <div className="App">
-      <Container className="main-container">
-        <img src={banner} alt="I need this now banner" className="banner"></img>
-        <form>
-          <label for="searchBar">Search: </label>
+    <div className="App" style={{ backgroundColor: "#92b4a7" }}>
+      <Container style={{ backgroundColor: "white" }} className="text-center">
+        <img
+          src={banner}
+          alt="I need this now banner"
+          className={"mx-auto"}
+        ></img>
+        <form className={"d-flex justify-content-center mb-4"}>
+          <label for="searchBar" className={"mx-2"}>
+            Search:{" "}
+          </label>
           <input
             id="searchbar"
             onChange={(e) => setSearchedWord(e.target.value)}
           ></input>
         </form>
-        {searchedWord}
-
-        <Product product={allProducts} />
+        <Row className="d-flex justify-content-center">
+          {searchedWord}
+          {allProducts.map((product) => (
+            <Product product={product} />
+          ))}
+        </Row>
       </Container>
     </div>
   );
--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,12 +1,39 @@
-const Product = (product) => {
-  const { name, description, img, url } = product;
+import Card from "react-bootstrap/Card";
+
+const Product = ({ product }) => {
+  const { id, name, description, img, url } = product;
 
   return (
-    <a href={url} className="product-box">
-      <p className="product-name">{name}</p>
-      <img src={img} alt="" className="product-img"></img>
-      <div className="product-description">{description}</div>
-    </a>
+    <Card
+      className={"m-2"}
+      style={{
+        width: "15rem",
+        backgroundColor: "#388697",
+        boxShadow: "#333 .1rem .2rem .2rem",
+      }}
+    >
+      <a key={id} href={url} className="product-box">
+        <Card.Img
+          style={{ height: "10rem", objectFit: "contain" }}
+          variant="top"
+          src={img}
+          alt={name}
+          className="mt-3"
+        ></Card.Img>
+        <Card.Body
+          style={{
+            backgroundColor: "white",
+            border: ".1rem solid #333",
+            boxShadow: "#333 .1rem .1rem .1rem",
+            borderRadius: "5%",
+          }}
+          className="m-2"
+        >
+          <Card.Title className="product-name">{name}</Card.Title>
+          <Card.Text className="product-description">{description}</Card.Text>
+        </Card.Body>
+      </a>
+    </Card>
   );
 };
 
